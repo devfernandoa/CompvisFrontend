30c019359e0e61b1508b99a3d860380d506ac32d
devfernandoa <fefealzueta@gmail.com>
2025-06-10 21:47:13 -0300

    vitoria

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      // any request to /api_placa will go to your FastAPI
-      '/api_placa': {
-        target: 'http://ec2-13-218-71-196.compute-1.amazonaws.com:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api_placa/, ''),
-      },
-    },
-  },
 })
 
